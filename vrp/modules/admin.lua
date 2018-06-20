2006203b31b61e4a56ff2b2e92bbae4e526df54c
DoubleRepo <34832750+DoubleRepo@users.noreply.github.com>
2018-06-20 23:02:39 +0200

    Warn user implementation.
    Simple warning system implementation.

--- vrp/modules/admin.lua
+++ vrp/modules/admin.lua
@@ -117,6 +117,23 @@ local function ch_removegroup(player,choice)
   end
 end
 
+local function ch_WarnPlayer(player,choice)
+  local user_id = vRP.getUserId(player)
+  if user_id ~= nil and vRP.hasPermission(user_id,"player.kick") then
+	vRP.prompt(player,"User id to Warn: ","",function(player,id)
+	id = parseInt(id)
+	  vRP.prompt(player,"Reason: ","",function(player,reason)
+        local source = vRP.getUserSource(id)
+        if source ~= nil then
+          vRP.WarnPlayer(player,source,reason)
+		else
+			vRPclient.notify(player,{"~r~No player id."})
+		end
+      end)
+    end)
+  end
+end
+
 local function ch_kick(player,choice)
   local user_id = vRP.getUserId(player)
   if user_id ~= nil and vRP.hasPermission(user_id,"player.kick") then
